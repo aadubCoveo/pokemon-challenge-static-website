c327c285cfeac0cbc6279b3f229f3f0c19ef1220
Alec Adub <aadub@coveo.com>
2021-02-05 17:23:05 -0500

    v7

--- js/coveo.extension.js
+++ js/coveo.extension.js
@@ -14,7 +14,7 @@ var CoveoExtension =
 /******/ 	function hotDownloadUpdateChunk(chunkId) {
 /******/ 		var script = document.createElement("script");
 /******/ 		script.charset = "utf-8";
-/******/ 		script.src = __webpack_require__.p + "2fed7c9-" + chunkId + "-wps-hmr.js";
+/******/ 		script.src = __webpack_require__.p + "c653ab5-" + chunkId + "-wps-hmr.js";
 /******/ 		if (null) script.crossOrigin = null;
 /******/ 		document.head.appendChild(script);
 /******/ 	}
@@ -28,7 +28,7 @@ var CoveoExtension =
 /******/ 			}
 /******/ 			try {
 /******/ 				var request = new XMLHttpRequest();
-/******/ 				var requestPath = __webpack_require__.p + "2fed7c9-wps-hmr.json";
+/******/ 				var requestPath = __webpack_require__.p + "c653ab5-wps-hmr.json";
 /******/ 				request.open("GET", requestPath, true);
 /******/ 				request.timeout = requestTimeout;
 /******/ 				request.send(null);
@@ -64,7 +64,7 @@ var CoveoExtension =
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "95ad25b765d05a5c666b";
+/******/ 	var hotCurrentHash = "8f543ff3ceea47a17219";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
